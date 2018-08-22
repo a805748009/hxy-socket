0ee061660cd9f6d28ffead9a103a70b6df4816d1
黄新宇 <805748009@qq.com>
2018-08-23 02:27:49 +0800

    修改加密套件

--- core/src/main/java/com/hxy/nettygo/result/base/ssl/SslFactory.java
+++ core/src/main/java/com/hxy/nettygo/result/base/ssl/SslFactory.java
@@ -1,10 +1,12 @@
 package com.hxy.nettygo.result.base.ssl;
 
 import java.io.File;
+import java.util.Arrays;
 
 import com.hxy.nettygo.result.base.config.ConfigForSSL;
 
 import io.netty.handler.ssl.ClientAuth;
+import io.netty.handler.ssl.IdentityCipherSuiteFilter;
 import io.netty.handler.ssl.SslContext;
 import io.netty.handler.ssl.SslContextBuilder;
 
@@ -14,6 +16,7 @@ import io.netty.handler.ssl.SslContextBuilder;
  */
 public class SslFactory {
 	private static  SslContext sslContext = null;
+	public static final String[] CIPHER_ARRAY = {"TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256", "TLS_DHE_RSA_WITH_AES_128_GCM_SHA256", "TLS_DHE_DSS_WITH_AES_128_GCM_SHA256"};
 
 	public static SslContext createSSLContext() throws Exception {
 		if (null == sslContext) {
@@ -21,7 +24,9 @@ public class SslFactory {
 				if (null == sslContext) {
 					 File certFilePath = new File(ConfigForSSL.CERTFILEPATH);
 					 File keyFilePath =  new File(ConfigForSSL.KEYFILEPATH);//此处需要PKS8编码的.key后缀文件
-					 sslContext = SslContextBuilder.forServer(certFilePath, keyFilePath).clientAuth(ClientAuth.NONE).build();
+					 sslContext = SslContextBuilder.forServer(certFilePath, keyFilePath)
+							 .clientAuth(ClientAuth.NONE).ciphers( Arrays.asList(CIPHER_ARRAY),IdentityCipherSuiteFilter.INSTANCE_DEFAULTING_TO_SUPPORTED_CIPHERS)//只允许用上面的三种128位加密套件，一般情况下去除这一行
+							 .build();
 				}
 			}
 		}
